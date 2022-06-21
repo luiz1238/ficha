14663a01b32463922dd9cc79629e5a360fcf2a57
luiz1238 <91709506+luiz1238@users.noreply.github.com>
2022-06-21 16:46:08 -0300

    Update spell.ts

--- src/pages/api/sheet/player/spell.ts
+++ src/pages/api/sheet/player/spell.ts
@@ -11,8 +11,9 @@ function handler(req: NextApiRequest, res: NextApiResponseServerIO) {
 
 async function handlePut(req: NextApiRequest, res: NextApiResponseServerIO) {
 	const player = req.session.player;
+        const npcId: number | undefined = req.body.npcId;
 
-	if (!player) {
+	if (!player || (player.admin && !npcId)) {
 		res.status(401).end();
 		return;
 	}
@@ -24,7 +25,6 @@ async function handlePut(req: NextApiRequest, res: NextApiResponseServerIO) {
 		return;
 	}
 
-	const npcId: number | undefined = req.body.npcId;
 
 	const playerId = npcId ? npcId : player.id;
 
@@ -43,8 +43,10 @@ async function handlePut(req: NextApiRequest, res: NextApiResponseServerIO) {
 
 async function handleDelete(req: NextApiRequest, res: NextApiResponseServerIO) {
 	const player = req.session.player;
+        const npcId: number | undefined = req.body.npcId; 
 
-	if (!player) {
+
+	if (!player || (player.admin && !npcId)) {
 		res.status(401).end();
 		return;
 	}
@@ -55,8 +57,6 @@ async function handleDelete(req: NextApiRequest, res: NextApiResponseServerIO) {
 		res.status(400).send({ message: 'spell ID is undefined.' });
 		return;
 	}
-
-	const npcId: number | undefined = req.body.npcId;
 
 	const playerId = npcId ? npcId : player.id;
 
